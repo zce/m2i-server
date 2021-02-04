7d8a5a63a0eba551fffa82137100524adcb02dd9
zce <w@zce.me>
2021-02-04 20:43:48 +0800

    feat: initial commit

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,5 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import chrome from 'chrome-aws-lambda'
-import puppeteer from 'puppeteer-core'
+import playwright from 'playwright-aws-lambda'
 import marked from 'marked'
 
 const defaultOptions = {
@@ -25,15 +24,9 @@ export default async (req: VercelRequest, res: VercelResponse): Promise<any> => 
   const html = options.template.replace('{{markdown}}', content.trim())
 
   // capture screenshot by puppeteer
-  const browser = await puppeteer.launch({
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless
-  })
-  const page = await browser.newPage()
-  await page.setViewport({ width: options.width, height: 80, deviceScaleFactor: 2 })
+  const browser = await playwright.launchChromium()
+  const page = await browser.newPage({ viewport: { width: options.width, height: 80 }, deviceScaleFactor: 2 })
   await page.setContent(html)
-  await page.waitFor(200) // wait for rendering
   const buffer = await page.screenshot({ fullPage: true })
   await browser.close()
 
