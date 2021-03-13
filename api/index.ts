2b514f6a428496a416ded902b936062ddbceca62
zce <w@zce.me>
2021-03-13 12:04:12 +0800

    feat: use chrome-aws-lambda

--- api/index.ts
+++ api/index.ts
@@ -1,12 +1,43 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import { launchChromium } from 'playwright-aws-lambda'
+import chromium from 'chrome-aws-lambda'
+import puppeteer from 'puppeteer-core'
 import marked from 'marked'
 
-const defaultOptions = {
+interface Options {
+  headless: boolean
+  executablePath: string
+  args: string[]
+}
+
+const defaultParams = {
   width: 600,
   scale: 2,
   template: '<link rel="stylesheet" href="https://cdn.zce.me/markdown.css">{{markdown}}'
 }
+
+const chromePaths = {
+  win32: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
+  darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+  linux: '/usr/bin/google-chrome'
+}
+
+// const edgePaths = {
+//   win32: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
+//   darwin: '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge',
+//   linux: '/usr/bin/microsoft-edge'
+// }
+
+const awsOptions = async (): Promise<Options> => ({
+  headless: chromium.headless,
+  executablePath: await chromium.executablePath,
+  args: chromium.args
+})
+
+const localOptions = (): Options => ({
+  headless: true,
+  executablePath: chromePaths[process.platform as keyof typeof chromePaths],
+  args: []
+})
 
 export default async (req: VercelRequest, res: VercelResponse): Promise<any> => {
   if (typeof req.body.markdown !== 'string' || req.body.markdown === '') {
@@ -15,19 +46,21 @@ export default async (req: VercelRequest, res: VercelResponse): Promise<any> => 
   }
 
   const markdown = req.body.markdown
-  const height = 80
   /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-  const width = ~~req.body.width || defaultOptions.width
-  const deviceScaleFactor = ~~req.body.scale || defaultOptions.scale
-  const template = req.body.template || defaultOptions.template
+  const height = 200
+  const width = ~~req.body.width || defaultParams.width
+  const deviceScaleFactor = ~~req.body.scale || defaultParams.scale
+  const template = req.body.template || defaultParams.template
   /* eslint-enable @typescript-eslint/strict-boolean-expressions */
 
   const content = marked(markdown)
-
   const html = template.replace('{{markdown}}', content.trim())
 
-  const browser = await launchChromium()
-  const page = await browser.newPage({ viewport: { width, height }, deviceScaleFactor })
+  const isAws = process.env.AWS_REGION == null
+  const options = isAws ? localOptions() : await awsOptions()
+  const browser = await puppeteer.launch(options)
+  const page = await browser.newPage()
+  await page.setViewport({ width, height, deviceScaleFactor })
   await page.setContent(html)
   const buffer = await page.screenshot({ fullPage: true })
   await browser.close()
