d3151531b1afcbe71cda237feaa67cb3fababc63
zce <w@zce.me>
2021-07-07 20:25:14 +0800

    chore: meta tweaks

--- api/index.ts
+++ api/index.ts
@@ -11,17 +11,18 @@ interface Options {
 
 const defaultParams = {
   width: 600,
+  height: 200,
   scale: 2,
   template: '<link rel="stylesheet" href="https://cdn.zce.me/markdown.css">{{markdown}}'
 }
 
-const chromePaths = {
+const chromiumPaths = {
   win32: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe',
   darwin: '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
   linux: '/usr/bin/google-chrome'
 }
 
-// const edgePaths = {
+// const chromiumPaths = {
 //   win32: 'C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe',
 //   darwin: '/Applications/Microsoft Edge.app/Contents/MacOS/Microsoft Edge',
 //   linux: '/usr/bin/microsoft-edge'
@@ -35,25 +36,26 @@ const awsOptions = async (): Promise<Options> => ({
 
 const localOptions = (): Options => ({
   headless: true,
-  executablePath: chromePaths[process.platform as keyof typeof chromePaths],
+  executablePath: chromiumPaths[process.platform as keyof typeof chromiumPaths],
   args: []
 })
 
 export default async (req: VercelRequest, res: VercelResponse): Promise<any> => {
-  if (typeof req.body.markdown !== 'string' || req.body.markdown === '') {
+  const { markdown } = req.body
+
+  if (typeof markdown !== 'string' || markdown === '') {
     // return if without redirect url.
     return res.status(400).send({ message: 'Bad Request: missing required `markdown`.' })
   }
 
-  const markdown = req.body.markdown
   /* eslint-disable @typescript-eslint/strict-boolean-expressions */
-  const height = 200
+  const height = defaultParams.height
   const width = ~~req.body.width || defaultParams.width
   const deviceScaleFactor = ~~req.body.scale || defaultParams.scale
   const template = req.body.template || defaultParams.template
   /* eslint-enable @typescript-eslint/strict-boolean-expressions */
 
-  const content = marked(markdown)
+  const content = marked(markdown.replace(/^---$.*^---$\s*/ms, ''))
   const html = template.replace('{{markdown}}', content.trim())
 
   const options = process.env.AWS_REGION == null ? localOptions() : await awsOptions()
